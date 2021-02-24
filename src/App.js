9141a27892b58cde55c0218fce1c360d24ae37e2
Zahid Hasan <jahidhasann67@gmail.com>
2021-02-24 22:25:16 +0600

    Component Tree

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import React from 'react';
 import './App.css';
+import MainComponent from './components/MainComponent';
 
 function App() {
   return (
     <div className="App">
-      
+      <MainComponent />
     </div>
   );
 }
