ed518408db4a2e2072f3b55d3bf8262b06f44976
Zahid Hasan <jahidhasann67@gmail.com>
2021-02-25 12:57:25 +0600

    Desining Menu Items

--- src/components/body/MenuItem.js
+++ src/components/body/MenuItem.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import { Card, CardImg } from 'react-bootstrap';
+import { CardBody, CardImgOverlay, CardTitle } from 'reactstrap';
+
 
 const MenuItem = props => {
     return (
         <div>
-
+        <Card style={{margin: "10px"}}>
+            <CardBody>
+                <CardImg 
+                with="100%" 
+                alt={props.dish.name} 
+                src={props.dish.image} 
+                style={{opacity: "0.5"}} />
+                <CardImgOverlay>
+                    <CardTitle>{props.dish.name}</CardTitle>
+                </CardImgOverlay>
+            </CardBody>
+        </Card>
         </div>
     );
 }
